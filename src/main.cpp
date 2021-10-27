ec8876938f005392de31fe58da309b277e1d23e0
JPig-Main <jumpei141107@gmail.com>
2021-10-27 20:48:35 +0900

    add cmath-library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <GL/glut.h>
 #include <iostream>
-#include <cmath>
 #include <map>
 using namespace std;
 
@@ -13,6 +12,7 @@ using namespace std;
 #include "cli/clock.h"
 #include "gli/tool.h"
 #include "gli/window.h"
+#include "gli/draw.h"
 
 void display();
 
@@ -22,40 +22,6 @@ int main(int argc, char *argv[])
     window_init(&argc, argv, new Xy(600, 480), display, 100);
 
     return 0;
-}
-
-// 針を描画
-void draw_clock_needle(float len, float angle, float lineWidth, Rgb *color)
-{
-    // BUG 線の太さが変わらない
-    auto center_pos = Gl::centerPos();
-
-    glBegin(GL_LINES);
-    Gl::color(color);
-    glVertex2i(center_pos->x, center_pos->y);
-    glVertex2i(
-        center_pos->x + (int)(len * sin(angle)),
-        center_pos->y - (int)(len * cos(angle)));
-    glEnd();
-}
-
-void draw_circle(Xy* center_pos, float round, Rgb* filling_color) {
-    int edge_num = 360;
-    float angle;
-
-    glBegin(GL_POLYGON);
-    Gl::color(filling_color);
-    
-    for (auto i = 0; i < edge_num; i++)
-    {
-        angle = n_ary_convertor(i, edge_num, M_PI * 2);
-        glVertex2i(
-            center_pos->x + (int)(round * sin(angle)),
-            center_pos->y - (int)(round * cos(angle))
-        );
-    }
-
-    glEnd();
 }
 
 // ウィンドウの表示内容を更新する関数
