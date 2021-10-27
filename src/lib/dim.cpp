8885880dd27cce8eafcac4338303b98baa6006ea
JPig-Main <jumpei141107@gmail.com>
2021-10-27 12:59:38 +0900

    座標のクラスを追加

---
+++ src/lib/dim.cpp
@@ -0,0 +1,7 @@
+#include "dim.h"
+
+Dim::Dim(int _x, int _y)
+{
+    x = _x;
+    y = _y;
+}
---
+++ src/lib/dim.h
@@ -0,0 +1,12 @@
+#ifndef _DIM_H_
+#define _DIM_H_
+
+class Dim
+{
+public:
+    int x;
+    int y;
+    Dim(int, int);
+};
+
+#endif
